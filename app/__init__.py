ee336a60c1764e9c12c78142c1e34fb4c57163bf
gitviini <gvinicius105@gmail.com>
2023-11-06 22:21:01 -0300

    upload statics

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,6 @@
 from flask import Flask, render_template, request, session, redirect, url_for, make_response, abort, flash
-from flask_cors import CORS
 
 app = Flask(__name__, template_folder='templates')
 app.secret_key = '123'
-CORS(app)
 
 from app.controlers.default import *
